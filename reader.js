82cedea89e55d5a4c6609899f33d76a53ae6f2d1
GangsterProgramer <ali.hamidigithub@gmail.com>
2024-09-15 15:25:34 +0330

    html

--- reader.js
+++ reader.js
@@ -1,6 +1,12 @@
-var MMDBReader = require('mmdb-reader');
-
-// Load synchronously
-var reader = new MMDBReader('./my-ip-data.mmdb');
-
-console.log(reader.lookup('8.8.8.8'));
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    git
+</head>
+<body>jjjj
+    <p style="display: none;" class="n" id="name">بزن به چاک از ای</p>
+    <script src="js/script.js"></script>
+</body>
+</html>
